b7f6d0865393e08c753733eaf71494a0280ab072
Kaitoeight <khanhbatha@gmail.com>
2023-06-03 03:24:46 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "đíu biết chúc gì",
+    introTitle: 'HappyBirthday',
+    introDesc: `cái này được tạo ra để bắt nhận quà sinh nhật`,
+    btnIntro: 'Grrrr',
+    title: 'Chọn một trong 2 món đồ',
+    desc: 'suy nghĩ kĩ trước khi chọn ',
+    btnYes: 'Thắt lưng siêu nhân biến hình',
+    btnNo: 'chong chóng tre',
+    question: 'quả là một lựa chọn sáng suốt! đáng khen.',
+    btnReply: 'Gửi câu trả lời',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Ê gửi qua mess á :)) đừng gửi trong này',
+    messDesc: 'cái thắt lưng siêu nhân biến hình thí thứ 2 tôi mới đưa được .',
+    btnAccept: 'Nhấn tiếp đi nhìn gì ',
+    messLink: 'https://www.facebook.com/messages/t/100040564341434' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100026297167008
+}
